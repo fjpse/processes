da644cdb995ea3376436dded4db12e550d446f13
Javier de <fjdepedro@gmail.com>
2020-06-28 22:22:49 +0000

    Inicio

---
+++ foo.erl
@@ -0,0 +1,22 @@
+-module(foo).
+-export([bar/0, bar/1, baz/0]).
+
+bar() ->
+    timer:sleep(500),
+    io:format("bar started~n"),
+    io:format("bar working~n"),
+    io:format("bar finished~n").
+
+bar(Pid) ->
+    Pid ! "bar started~n",
+    Pid ! "bar working~n",
+    Pid ! "bar finished~n".
+
+baz() ->
+    receive
+        stop ->
+            io:format("stopped~n");
+        Msg ->
+            io:format("got: ~s~n", [Msg]),
+            baz()
+    end.
